d8354482c753badca807f782a2ad7df525872b68
imsankar <sankar.zabu@gmail.com>
2018-08-09 06:31:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 # Specify the provider and access details
 provider "aws" {
-  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
 }
-
 provider "archive" {}
 
 data "archive_file" "zip" {
