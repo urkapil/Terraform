61ef3c1da475660440bb69b24f71f63fb1a1b308
imsankar <sankar.zabu@gmail.com>
2018-08-09 06:34:29 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "aws_region" {
+  description = "access key of the account"
+  default     = "us-east-1"
+}
+
+variable "aws_region" {
+  description = "Secret key of the account"
+  default     = "us-east-1"
+}
+
+variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "us-east-1"
 }
